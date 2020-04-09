ffebe3c5ae82caa6e1fe15b130f119c0b10ccf8a
lengyelluk <lengyel.luk@gmail.com>
2020-04-09 21:32:31 +0200

    card grid modified to use semantic ui react

--- client/src/components/CardList/index.js
+++ client/src/components/CardList/index.js
@@ -1,27 +1,30 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Grid, Image, Container, Header } from 'semantic-ui-react'
 import './style.css'
 
 export default function CardList(props) {
 	const cards = props.cards
 	
 	return (
-		<div className='grid'>
+		<Grid>
 			{cards && cards.map(renderCard)}
-		</div>
+		</Grid>
 	)
 }
 
 function renderCard(card, key) {
 	
 	return (
-		<div className='container' key={key}>
-			<Link to={`/cards/${card._id}`}>
-				<h3>{card.title}</h3>
-				<img src={require("../../assets/images/flat.jpg")} className='card-image' alt="flat" />
-				<h3><span className="price"> {card.price.value} {card.price.currency}/month</span></h3>
-				<p>{card.street}, {card.district}</p>
-			</Link>
-		</div>
+		<Grid.Column key={key} computer={5} tablet={8} mobile={16}>
+			<Container id="card-container">
+				<Header as='h3'>{card.title}</Header>
+				<Link to={`/cards/${card._id}`}>
+					<Image src={require("../../assets/images/flat.jpg")} className='card-image' alt="flat" />
+				</Link>
+				<Header as='h3'>{card.street}, {card.district}</Header>
+				<p> {card.price.value} {card.price.currency}/month</p>
+			</Container>
+		</Grid.Column>
 	)
 }
