c915fa66b3b3dfb49f1e2fc2286c75f0fc51065f
lengyelluk <lengyel.luk@gmail.com>
2020-04-11 13:16:51 +0200

    in about page shift of table from two columns to one using media query

--- client/src/screen/About/index.js
+++ client/src/screen/About/index.js
@@ -1,28 +1,33 @@
 import React, { PureComponent } from 'react'
 import './style.css'
-import { Image, List, Button} from 'semantic-ui-react'
+import { Container, Header, Image, List, Button, Responsive } from 'semantic-ui-react'
 
 class AboutScreen extends PureComponent {
     render() {
         return (
-            <div className='content'>
-                <h1>About My Flat App</h1>
-                <h3>This page was built to experience at first hand web development using shift-left approach</h3> 
-                <p>What does shift-left mean? In simple terms, it means that testing is not performed at the last stage of software development lifecycle but it starts as close
+            <Container>
+            <div id='about-content'>
+                <Header as='h1'>About My Flat App</Header>
+                <Header as='h3'>This page was built to experience at first hand web development using shift-left approach</Header> 
+                <p>What does <strong>shift-left</strong> mean? In simple terms, it means that testing is not performed at the last stage of software development lifecycle but it starts as close
                     to the beginning of the project as possible.</p>
-                <p>This is not a fully working model of web page. The goal was to try shift-left approach in web development</p>
+                <p>This is not a fully working model of a web page. The goal was to try shift-left approach in web development</p>
                 <br></br>
-                <p><strong>Shift-left == test early && test often.</strong></p> 
+                <q><strong>Shift-left means test early and test often.</strong></q> 
                 <br></br>
                 <p className='float-left'></p>
                 <br></br>
-                <List divided verticalAlign='middle'>
+                <Responsive as={List} className={'divided'} minWidth={1080}>
                     <List.Item>
                         <List.Content floated='left'>
-                        <strong>Technology stack used</strong>
+                            <List.Header>
+                                <strong>Technology stack used</strong>
+                            </List.Header>
                         </List.Content>
                         <List.Content floated='right'>
-                        <strong>Type of automated tests used during development</strong>
+                            <List.Header>
+                                <strong>Type of automated tests used during development</strong>
+                            </List.Header>
                         </List.Content>
                     </List.Item>
                     <List.Item>
@@ -49,8 +54,57 @@ class AboutScreen extends PureComponent {
                             <strong>UI tests</strong> - to verify E2E functionality after each deployment
                         </List.Content>
                     </List.Item>
-                </List>
+                </Responsive>
+                <Responsive id='about-first-list' as={List} className={'divided'} maxWidth={1079}>
+                    <List.Item>
+                        <List.Content>
+                            <List.Header>
+                                <strong>Technology stack used</strong>
+                            </List.Header>
+                        </List.Content>
+                    </List.Item>
+                    <List.Item>
+                        <List.Content>
+                        Built using <strong>MERN stack</strong> (MongoDB, ExpressJS, ReactJS, NodeJS)
+                        </List.Content>
+                    </List.Item>
+                    <List.Item>
+                        <List.Content>
+                            Deployed using <strong>Heroku</strong> and <strong>Travis CI</strong>
+                        </List.Content>
+                    </List.Item>
+                    <List.Item>
+                        <List.Content>
+                            Tests written in <strong>Jest</strong> and <strong>Selenium</strong>
+                        </List.Content>
+                    </List.Item>
+                </Responsive>
+                <Responsive as={List} className={'divided'} maxWidth={1079}>
+                    <List.Item>
+                        <List.Content>
+                            <List.Header>
+                                <strong>Type of automated tests used during development</strong>
+                            </List.Header>
+                        </List.Content>
+                    </List.Item>
+                    <List.Item>
+                        <List.Content>
+                            <strong>Unit tests</strong> - executed after each push to common repository
+                        </List.Content>
+                    </List.Item>
+                    <List.Item>
+                        <List.Content>
+                            <strong>API tests</strong> - each service tested with automated tests
+                        </List.Content>
+                    </List.Item>
+                    <List.Item>
+                        <List.Content>
+                            <strong>UI tests</strong> - to verify E2E functionality after each deployment
+                        </List.Content>
+                    </List.Item>
+                </Responsive>
             </div>
+            </Container>
         )
     }
 }
